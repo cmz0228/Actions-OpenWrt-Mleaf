ff1a9d0a50b4a563b598cb6c9c9750d0245c4ab6
ywgf88 <ywgf@gmail.com>
2020-02-07 13:51:26 +0800

    new one

---
+++ pw.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i '3a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#./scripts/feeds update -a && ./scripts/feeds install -a
